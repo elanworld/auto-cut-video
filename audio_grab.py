fa510a41436ae26261036ba7e5294d33a252dc5b
elanworld <elanworld@163.com>
2021-01-03 18:54:14 +0800

    python: 主动降噪 增加输入设备和处理算法

--- audio_grab.py
+++ audio_grab.py
@@ -1,12 +1,39 @@
 import time
 
+import numpy as np
 from pyaudio import PyAudio, paInt16
 
 framerate = 16000
-NUM_SAMPLES = 50
+NUM_SAMPLES = 100
 channels = 1
 sampwidth = 2
 TIME = 10
+
+
+def get_input():
+    p = PyAudio()
+    info = p.get_host_api_info_by_index(0)
+    numdevices = info.get('deviceCount')
+    devices = []
+    for i in range(0, numdevices):
+        devices.append(p.get_device_info_by_host_api_device_index(0, i))
+        print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+    return devices[1]
+
+
+@DeprecationWarning
+def frame_deal(data):
+    """
+    信号反向处理
+    :param data:
+    :return:
+    """
+    new_data = b""
+    for c in data:
+        _n = -(int(c) - 128) + 128
+        b = bytes(_n)
+        new_data += b
+    return new_data
 
 
 def noise_replay():
@@ -18,18 +45,22 @@ def noise_replay():
     pa = PyAudio()
     stream = pa.open(format=paInt16, channels=1,
                      rate=framerate, input=True,
-                     frames_per_buffer=NUM_SAMPLES)
+                     frames_per_buffer=NUM_SAMPLES, input_device_index=1)
     out_stream = pa.open(format=paInt16, channels=1,
-                         rate=framerate, output=True)
+                         rate=framerate, output=True, output_device_index=5)
     count = 0
-    while count < TIME * 200000:  # 控制录音时间
+    while count < TIME * 200000:
         start = time.clock()
-        string_audio_data = stream.read(NUM_SAMPLES)
-        out_stream.write(string_audio_data)
+        data = stream.read(NUM_SAMPLES)
+        fromstring = np.fromstring(data, dtype=np.int16)
+        negative = np.negative(fromstring)
+        negative_data = negative.tostring()
+        out_stream.write(negative_data)
         print(time.clock() - start)
         count += 1
         print('.')
 
 
 if __name__ == '__main__':
+    get_input()
     noise_replay()
